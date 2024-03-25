7095d4444d1082138b482e586a8a4c43c7fa6202
Elena Pakhomova <pakhomova.e@gmail.com>
2024-03-25 21:15:02 +0300

    added other methods

--- lesson4/test_string_utils.py
+++ lesson4/test_string_utils.py
@@ -4,25 +4,28 @@ from string_utils import StringUtils
 
 str = StringUtils()
 
+
 # Tests def capitilize(self, string: str) -> str
-
-
 @pytest.mark.parametrize("input_str, result", [
     ("первый", "Первый"),
     ("rere", "Rere"),
-    ("Первый", "Первый"),
     ("первый тест", "Первый тест"),
     ("в процессе форка создается копия всех файлов.\
     эта копия сохраняется.", "В процессе форка создается копия всех файлов.\
-    эта копия сохраняется.")])
+    эта копия сохраняется."),
+    ("without whitespace", "Without whitespace")
+    ])
 def test_input_tex_capitilize(input_str, result):
-    # str = StringUtils()
     res = str.capitilize(input_str)
     assert res == result
 
 
-@pytest.mark.parametrize("symbol, result", [("!", "!"), ("", ""), (" ", " ")])
-def test_str_capitilize(symbol, result):
+@pytest.mark.parametrize("symbol, result", [
+    ("!", "!"),
+    ("", ""),
+    (" ", " ")
+    ])
+def test_symbols_capitilize(symbol, result):
     res = str.capitilize(symbol)
     assert res == result
 
@@ -43,8 +46,6 @@ def test_empty_str_capitilize():
 
 
 # Test def trim(self, string: str) -> str
-
-
 @pytest.mark.parametrize("input_str, result", [
     ("Without whitespace", "Without whitespace"),
     (" Grom", "Grom"), ("  Лето", "Лето"),
@@ -67,25 +68,16 @@ def test_whitespaces_word_finish_trim(input_str, result):
 
 
 # Test def to_list(self, string: str, delimeter=",") -> list[str]
-
 @pytest.mark.parametrize("input_str, result", [
-    # без пробелов перед символом латиница
     ("a,b,c,f", ["a", "b", "c", "f"]),
-    # с пробелов перед символом латиница
     (" a, b, c, f", [" a", " b", " c", " f"]),
-    # без пробелов перед символом кириллица
     ("а,б,в,г", ["а", "б", "в", "г"]),
-    # с пробелов перед символом кириллица
     (" а, б, в, г", [" а", " б", " в", " г"]),
-    # цифры
     ("1,2,3,4", ["1", "2", "3", "4"]),
-    # больше 1 слова кириллица
     ("Онлайн регистрация,Личный кабинет,Приложение и интернет",
      ["Онлайн регистрация", "Личный кабинет", "Приложение и интернет"]),
-    # больше 1 слова латиница
     ("Online Registration,Personal Office,App and Internet",
      ["Online Registration", "Personal Office", "App and Internet"]),
-    # символы
     ("!!!,####,8888,~~~~,=", ["!!!", "####", "8888", "~~~~", "="])
      ])
 def test_symbols_to_list(input_str, result):
@@ -95,19 +87,12 @@ def test_symbols_to_list(input_str, result):
 
 @pytest.mark.parametrize("input_str, delimeter, result", [
     ("a:b:c:d:f", ":", ["a", "b", "c", "d", "f"]),
-    # разделитель : кириллица
     ("а:б:в:г", ":", ["а", "б", "в", "г"]),
-    # разделитель ! латиница
     ("a!b!c!d!f", "!", ["a", "b", "c", "d", "f"]),
-    # разделитель ! кириллица
     ("а!б!в!г", "!", ["а", "б", "в", "г"]),
-    # разделитель ! латиница
     ("a.b.c.d.f", ".", ["a", "b", "c", "d", "f"]),
-    # разделитель ! кириллица
     ("а.б.в.г", ".", ["а", "б", "в", "г"]),
-    # разделитель ! латиница
     ("a b c d f", " ", ["a", "b", "c", "d", "f"]),
-    # разделитель ! кириллица
     ("а б в г", " ", ["а", "б", "в", "г"])
 ])
 def test_symbols_with_delimeter(input_str, delimeter, result):
@@ -115,47 +100,47 @@ def test_symbols_with_delimeter(input_str, delimeter, result):
     assert res == result
 
 
-# =======================================
-# WARNING Вопрос - почему через print выводит ок, а при тесте дает ошибку????
-# @pytest.mark.parametrize("input_str, result", [
-#     ("12345", ["12345"])
-#     # ("a b c d f", ["a b c d f"])
-# ])
-# def test_without_delimeter_to_list(input_str, delimeter, result):
-#     res = str.to_list(input_str, delimeter, result)
-#     assert res == result
+def test_string_without_delimeter_to_list():
+    res = str.to_list("12345")
+    assert res == ["12345"]
 
 
-# print(str.to_list("12345"))
-# print(str.to_list("a b c d f"))
-# =======================================
+def test_string_without_delimeter_to_list2():
+    res = str.to_list("a b c d f")
+    assert res == ["a b c d f"]
+
+
+def test_without_delimeter_to_list2():
+    with pytest.raises(ValueError):
+        str.to_list("12345", "")
+
 
 def test_none_argument_to_list():
     with pytest.raises(TypeError):
         str.to_list()
 
 
-# Должна появлять ошибка, что передайте верный аргумент - лист
-@pytest.mark.skip
+@pytest.mark.skip(reason="должна быть ошибка - неверный аргумент")
 def test_none_to_list():
     with pytest.raises(AttributeError):
         str.to_list("")
 
 
-@pytest.mark.skip
+@pytest.mark.skip(reason="должна быть ошибка - неверный аргумент")
 def test_whitespace_to_list():
     with pytest.raises(AttributeError):
         str.to_list(" ")
 
 
 # def contains(self, string: str, symbol: str) -> bool
-
 @pytest.mark.parametrize("input_str, symbol, result", [
     ("Marmelad", "a", True),
     ("Marmelad", "y", False),
     ("Мармелад", "р", True),
     ("Мармелад", "ы", False),
     ("Marmasdef Llqzxcc", "z", True),
+    ("Marmasdef Llqzxcc", "c", True),
+    ("рлткыыат лпвпртрн", "ы", True),
     ("Marmasdef Llqzxcc", "v", False),
     ("Marmasdef Llqzxcc", "qzx", True),
     ("Marmasdef Llqzxcc", "qzy", False)
@@ -165,7 +150,6 @@ def test_string_contains(input_str, symbol, result):
     assert res == result
 
 
-# почему метод не ищет символы внезависимости от upper/lower case???
 @pytest.mark.skip(reason="реализовать поиск не чувствительный к регистру")
 @pytest.mark.parametrize("input_str, symbol, result", [
     ("Marmelad", "E", True),
@@ -189,11 +173,148 @@ def test_empty_argument1_str_contains():
         str.contains("gjdlfgjdfgjd")
 
 
-# def test_empty_argument2_str_contains():
-#     with pytest.raises(TypeError):
-#         str.contains("")
+# def delete_symbol(self, string: str, symbol: str) -> str
+@pytest.mark.parametrize("input_str, symbol, result", [
+    ("This", "s", "Thi"),
+    ("This simple technique is used", "s", "Thi imple technique i ued"),
+    ("This simple technique is used", "is", "Th simple technique  used"),
+    ("This simple technique is used", "T", "his simple technique is used")
+])
+def test_delete_symbol(input_str, symbol, result):
+    res = str.delete_symbol(input_str, symbol)
+    assert res == result
 
 
-# print(str.contains("Marmasdef Llqzxcc", "qzx"))
-# print(str.contains("Marmasdef Llqzxcc", "llq"))
-# print(str.contains())
+@pytest.mark.parametrize("input_str, symbol, result", [
+    ("", "", ""),
+    (" ", " ", "")
+])
+def test_empty_arguments_delete_symbol(input_str, symbol, result):
+    res = str.delete_symbol(input_str, symbol)
+    assert res == result
+
+
+def test_none_arguments_delete_symbol():
+    with pytest.raises(TypeError):
+        str.delete_symbol()
+
+
+# def starts_with(self, string: str, symbol: str) -> bool
+@pytest.mark.parametrize("input_str, symbol, result", [
+    ("This", "T", True),
+    ("This", "s", False),
+    ("This", "t", False),
+    ("simple technique is used", "s", True),
+    ("simple technique is used", "t", False),
+    ("Периметр", "П", True),
+    ("сторона", "а", False),
+    ("Как изменится периметр", "К", True),
+    ("Как изменится периметр", "к", False)
+])
+def test_starts_with(input_str, symbol, result):
+    res = str.starts_with(input_str, symbol)
+    assert res == result
+
+
+def test_none_arguments_starts_with():
+    with pytest.raises(TypeError):
+        str.starts_with()
+
+
+def test_only_one_arguments_starts_with():
+    with pytest.raises(TypeError):
+        str.starts_with("")
+
+
+#  def end_with(self, string: str, symbol: str) -> bool
+@pytest.mark.parametrize("input_str, symbol, result", [
+    ("This", "s", True),
+    ("This", "T", False),
+    ("This", "S", False),
+    ("simple technique is used", "d", True),
+    ("simple technique is used", "e", False),
+    ("Периметр", "р", True),
+    ("сторона", "с", False),
+    ("Как изменится периметр", "р", True),
+    ("Как изменится периметр", "к", False)
+])
+def test_end_with(input_str, symbol, result):
+    res = str.end_with(input_str, symbol)
+    assert res == result
+
+
+def test_none_arguments_end_with():
+    with pytest.raises(TypeError):
+        str.starts_with()
+
+
+def test_only_one_arguments_end_with():
+    with pytest.raises(TypeError):
+        str.starts_with("")
+
+
+# def is_empty(self, string: str) -> bool
+@pytest.mark.parametrize("input_str, result", [
+    ("", True),
+    ("    ", True),  # являет символ пробел не символом?
+    ("This", False),
+    ("simple technique is used", False)
+])
+def test_is_empty_with(input_str, result):
+    res = str.is_empty(input_str)
+    assert res == result
+
+
+def test_none_arguments_is_empty():
+    with pytest.raises(TypeError):
+        str.is_empty()
+
+
+# def list_to_string(self, lst: list, joiner=", ") -> str
+@pytest.mark.parametrize("input_list, result", [
+    (["a", "b", "c", "f"], "a, b, c, f"),
+    ([" a", " b", " c", " f"], " a,  b,  c,  f"),
+    (["а", "б", "в", "г"], "а, б, в, г"),
+    ([" а", " б", " в", " г"], " а,  б,  в,  г"),
+    (["1", "2", "3", "4"], "1, 2, 3, 4"),
+    (["Онлайн регистрация", "Личный кабинет", "Приложение и интернет"],
+     "Онлайн регистрация, Личный кабинет, Приложение и интернет"),
+    (["Online Registration", "Personal Office", "App and Internet"],
+     "Online Registration, Personal Office, App and Internet"),
+    (["!!!", "####", "8888", "~~~~", "="], "!!!, ####, 8888, ~~~~, =")
+     ])
+def test_list_to_string(input_list, result):
+    res = str.list_to_string(input_list)
+    assert res == result
+
+
+@pytest.mark.parametrize("input_str, delimeter, result", [
+    (["a", "b", "c", "d", "f"], ":", "a:b:c:d:f"),
+    (["а", "б", "в", "г"], ":", "а:б:в:г"),
+    (["a", "b", "c", "d", "f"], "!", "a!b!c!d!f"),
+    (["а", "б", "в", "г"], "!", "а!б!в!г"),
+    (["a", "b", "c", "d", "f"], ".", "a.b.c.d.f"),
+    (["а", "б", "в", "г"], ".", "а.б.в.г"),
+    (["a", "b", "c", "d", "f"], " ", "a b c d f")
+])
+def test_list_to_string_with_delimeter(input_str, delimeter, result):
+    res = str.list_to_string(input_str, delimeter)
+    assert res == result
+
+
+def test_none_argument_list_to_string():
+    with pytest.raises(TypeError):
+        str.list_to_string()
+
+
+# Нет смысла возвращать пустую строку, должна быть ошибка
+@pytest.mark.skip(reason="должна быть ошибка - неверный аргумент")
+def test_empty_argument_list_to_string():
+    with pytest.raises(AttributeError):
+        str.list_to_string("")
+
+
+@pytest.mark.skip(reason="должна быть ошибка - неверный аргумент")
+def test_whitespace_list_to_string():
+    with pytest.raises(AttributeError):
+        str.list_to_string(" ")
