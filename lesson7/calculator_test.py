48b1870db04216dde74a04607e800573bada3cd2
Elena Pakhomova <pakhomova.e@gmail.com>
2024-04-19 23:20:17 +0300

    changed folder structure

--- lesson7/calculator_test.py
+++ lesson7/calculator_test.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-from hw7_pages.CalculatorPage import Calculator
+from pages.CalculatorPage import Calculator
 
 spinner_time = 5
 
