0b558dbfd0fb5bc0197f8e306c70f5ec66ffebfb
Elena Pakhomova <pakhomova.e@gmail.com>
2024-05-13 20:47:58 +0300

    add not final test version

--- lesson9/EmployeeApi.py
+++ lesson9/EmployeeApi.py
@@ -1,12 +1,20 @@
 import requests
+import allure
 
 
 class EmployeeApi:
 
+    user = 'bloom'
+    password = 'fire-fairy'
+
     def __init__(self, url):
         self.url = url
 
-    def get_token(self, user='bloom', password='fire-fairy'):
+    @allure.step("api.получить токен авторизации")
+    def get_token(self, user=user, password=password) -> str:
+        """
+        Метод получает токен для авторизации.
+        """
         creds = {
             'username': user,
             'password': password
@@ -14,10 +22,15 @@ class EmployeeApi:
         resp = requests.post(self.url + '/auth/login', json=creds)
         return resp.json()["userToken"]
 
+    @allure.step("api.создать нового сотрудника компании")
     def create_employee(
-            self, company_id, first_name, last_name, email, isActive, id=1,
+            self, company_id: int, first_name: str, last_name: str,
+            email: str, isActive: bool, id=1,
             middle_name='', url='', phone='',
-            birthdate='2005-05-03T11:19:37.153Z'):
+            birthdate='2005-05-03T11:19:37.153Z') -> dict:
+        """
+        Метод создает нового сотрудника компании.
+        """
         creds = {
             'id': id,
             'firstName': first_name,
@@ -36,10 +49,12 @@ class EmployeeApi:
             self.url + '/employee', headers=my_headers, json=creds)
         return resp.json()
 
+    @allure.step("api.создать несколько - {num_emp} новых сотрудников компании")
     def create_list_employee_get_list_id(
-            self, num_emp, company_id, first_name, last_name, email, isActive, id=1,
+            self, num_emp, company_id, first_name, last_name,
+            email, isActive, id=1,
             middle_name='', url='', phone='',
-            birthdate='2005-05-03T11:19:37.153Z'):
+            birthdate='2005-05-03T11:19:37.153Z') -> list:
         list_new_emp = []
         list_new_emp_id = []
         creds = {
@@ -68,96 +83,85 @@ class EmployeeApi:
 
         return list_new_emp_id
 
+    @allure.step("api.создать сотрудника без токена авторизации")
     def create_employee_without_auth_token(
-            self, company_id, first_name, last_name, email, isActive, id=1,
-            middle_name='', url='', phone='',
-            birthdate='2005-05-03T11:19:37.153Z'):
-        creds = {
-            'id': id,
-            'firstName': first_name,
-            'lastName':  last_name,
-            'middleName': middle_name,
-            'companyId': company_id,
-            'email': email,
-            'url': url,
-            'phone': phone,
-            'birthdate': birthdate,
-            'isActive': isActive
-        }
+            self, company_id: int, dict_creds_emp: dict) -> dict:
+        """
+        Метод для проверки возможности создания сотрудника компании
+        без токена авторизации.
+        """
         resp = requests.post(
-            self.url + '/employee', json=creds)
+            self.url + '/employee', json=dict_creds_emp)
         return resp.json()
 
-    def create_employee_without_body(self):
+    @allure.step("api.создать сотрудника без тела запроса")
+    def create_employee_without_body(self) -> dict:
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
         resp = requests.post(
             self.url + '/employee', headers=my_headers)
         return resp.json()
 
-    def get_list_employee(self, params_to_add):
+    @allure.step("api.получить список сотрудников по параметру - {params}")
+    def get_list_employee(self, params_to_add) -> list:
+        """
+        Метод возвращает список словарей с данными сотрудников,
+        соответствующих определенному параметру.
+        """
         resp = requests.get(self.url + '/employee', params=params_to_add)
         return resp.json()
 
-    def get_employee_by_id(self, emp_id):
+    @allure.step("api.получить сотрудника по id")
+    def get_employee_by_id(self, emp_id: int) -> dict:
+        """
+        Метод получает словарь с информацией о сотруднике по id.
+        """
         resp = requests.get(self.url + '/employee/' + str(emp_id))
         return resp.json()
 
-    def get_employee_by_id_without_id(self):
+    @allure.step("api.получить сотрудника по id без id")
+    def get_employee_by_id_without_id(self) -> dict:
         resp = requests.get(self.url + '/employee/')
         return resp.json()
 
+    @allure.step("api.изменить информацию о сотруднике - {cred}")
     def change_info_employee(
-            self, emp_id, last_name, email, url, phone, is_active):
-        cred = {
-            'lastName': last_name,
-            'email': email,
-            'url': url,
-            'phone': phone,
-            'isActive': is_active
-        }
+            self, emp_id: int, api_creds_emp: dict) -> dict:
+        """
+        Метод находит сотрудника по id и изменяет значение полей.
+        """
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
 
         resp = requests.patch(
             self.url + '/employee/' + str(emp_id), headers=my_headers,
-            json=cred)
+            json=api_creds_emp)
         return resp.json()
 
+    @allure.step("api.изменить информацию о сотруднике без токена авторизации")
     def change_info_employee_without_auth_token(
-            self, emp_id, last_name, email, url, phone, is_active):
-        cred = {
-            'lastName': last_name,
-            'email': email,
-            'url': url,
-            'phone': phone,
-            'isActive': is_active
-        }
+            self, emp_id: int, api_creds_emp: dict) -> dict:
 
         resp = requests.patch(
             self.url + '/employee/' + str(emp_id),
-            json=cred)
+            json=api_creds_emp)
         return resp.json()
 
+    @allure.step("api.изменить информацию о сотруднике без id")
     def change_info_employee_without_id(
-            self, last_name, email, url, phone, is_active):
-        cred = {
-            'lastName': last_name,
-            'email': email,
-            'url': url,
-            'phone': phone,
-            'isActive': is_active
-        }
+            self, api_creds_emp: dict) -> dict:
+  
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
 
         resp = requests.patch(
             self.url + '/employee/', headers=my_headers,
-            json=cred)
+            json=api_creds_emp)
         return resp.json()
 
+    @allure.step("api.изменить информацию о сотруднике без тела запроса")
     def change_info_employee_without_body(
-            self, emp_id, last_name, email, url, phone, is_active):
+            self, emp_id: int) -> dict:
 
         my_headers = {}
         my_headers["x-client-token"] = self.get_token()
