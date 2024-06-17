bee5df149efdedc8382fc3cf4ce2e003d9140b3f
pakhomova-ev <pakhomova.e@gmail.com>
2024-06-17 19:47:01 +0300

    refactor 6.1: added file conftest.py, added file configuration.py

--- lesson6.1/test_fill_form.py
+++ lesson6.1/test_fill_form.py
@@ -1,56 +1,47 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from configuration import *
 
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
-    driver.maximize_window()
-    driver.get(
-        "https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
-    yield driver
-    driver.quit()
 
 
 background_color_alert_danger = 'rgba(248, 215, 218, 1)'
 background_color_alert_success = 'rgba(209, 231, 221, 1)'
 
-full_id_list = {
+full_field_list = {
         "first-name": "Иван", "last-name": "Петров",
         "address": "Ленина, 55-3", "zip-code": "",
         "city": "Москва", "country": "Россия",
         "e-mail": "test@skypro.com", "phone": "+7985899998787",
         "job-position": "QA", "company": "SkyPro"}
-id_list = ["first-name", "last-name", "address", "city", "country",
+field_list = ["first-name", "last-name", "address", "city", "country",
            "e-mail", "phone", "job-position", "company"]
 
 
-def test_background_color_unfilled_elements_in_form(driver):
+def test_background_color_unfilled_elements_in_form(driver_chrome):
+    driver_chrome.get(URL_FILL_FORM)
 
-    for id, value in full_id_list.items():
-        driver.find_element(By.NAME, id).send_keys(value)
+    for id, value in full_field_list.items():
+        driver_chrome.find_element(By.NAME, id).send_keys(value)
 
-    driver.find_element(By.CSS_SELECTOR, "button.btn-outline-primary").click()
-
-    zip_code_check = driver.find_element(
-        By.NAME, "zip-code").value_of_css_property("background-color")
+    WebDriverWait(driver_chrome, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-outline-primary"))).click()
+ 
+    zip_code_check = driver_chrome.find_element(
+        By.ID, "zip-code").value_of_css_property("background-color")
 
     assert zip_code_check == background_color_alert_danger
 
 
-def test_background_color_filled_elements_in_form(driver):
+def test_background_color_filled_elements_in_form(driver_chrome):
+    driver_chrome.get(URL_FILL_FORM)
 
-    for id, value in full_id_list.items():
-        driver.find_element(By.NAME, id).send_keys(value)
+    for id, value in full_field_list.items():
+        driver_chrome.find_element(By.NAME, id).send_keys(value)
 
-    driver.find_element(By.CSS_SELECTOR, "button.btn-outline-primary").click()
+    WebDriverWait(driver_chrome, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn-outline-primary"))).click()
 
-    for i in id_list:
-        i_check = driver.find_element(By.ID, i).value_of_css_property(
+    for i in field_list:
+        i_check = driver_chrome.find_element(By.ID, i).value_of_css_property(
             "background-color")
 
         assert i_check == background_color_alert_success
