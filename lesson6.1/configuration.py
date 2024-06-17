fe2a78108254722b308365ac337296662fbc06b0
pakhomova-ev <pakhomova.e@gmail.com>
2024-06-17 20:04:24 +0300

    added files

--- lesson6.1/configuration.py
+++ lesson6.1/configuration.py
@@ -2,3 +2,4 @@ URL_FILL_FORM = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
 URL_CALC = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
 URL_SHOP = "https://www.saucedemo.com/"
 
+full_field_list = {}
--- lesson6.1/conftest.py
+++ lesson6.1/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 from selenium import webdriver
+import allure
 
-
+@allure.step("open browser")
 @pytest.fixture
 def driver_chrome():
     driver = webdriver.Chrome()    
