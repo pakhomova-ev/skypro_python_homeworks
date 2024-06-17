c1c72d4eaac25bee2a156cb50400fcffc8c4276c
pakhomova-ev <pakhomova.e@gmail.com>
2024-06-17 19:51:05 +0300

    add new files into lesson6.1

---
+++ lesson6.1/configuration.py
@@ -0,0 +1,4 @@
+URL_FILL_FORM = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+URL_CALC = "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+URL_SHOP = "https://www.saucedemo.com/"
+
